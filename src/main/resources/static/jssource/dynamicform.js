2cc0e885f7eda2f454ea0cbe571686e30121bfef
Ruslan Zosimov <ruslan543213@gmail.com>
2018-06-14 23:14:42 +0300

    NoW Day

--- src/main/resources/static/jssource/dynamicform.js
+++ src/main/resources/static/jssource/dynamicform.js
@@ -21,13 +21,31 @@ $(function () {
     });
 });
 
-
-
-function numValid(input) {
+function numValidDouble(input) {
     $(input).keydown(function (event) {
         // Разрешаем: backspace, delete, tab, escape , "."
         if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
             event.keyCode == 110 || event.keyCode == 190 ||
+            // Разрешаем: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) ||
+            // Разрешаем: home, end, влево, вправо
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+            // Ничего не делаем
+            return;
+        }
+        else {
+            // Обеждаемся, что это цифра, и останавливаем событие keypress
+            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+                event.preventDefault();
+            }
+        }
+    });
+};
+
+function numValid(input) {
+    $(input).keydown(function (event) {
+        // Разрешаем: backspace, delete, tab, escape ,
+        if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
             // Разрешаем: Ctrl+A
             (event.keyCode == 65 && event.ctrlKey === true) ||
             // Разрешаем: home, end, влево, вправо
